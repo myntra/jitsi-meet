f0de3562847a930756cdf54e37e48373c4e1cd10
300026733 <kumar.swetank@myntra.com>
2020-12-28 20:14:06 +0530

    Reverted matrix url change

--- react/features/base/settings/constants.js
+++ react/features/base/settings/constants.js
@@ -3,4 +3,4 @@
 /**
  * The default server URL to open if no other was specified.
  */
-export const DEFAULT_SERVER_URL = 'https://matrixlive.myntra.com';
+export const DEFAULT_SERVER_URL = 'https://meet.jit.si';
