c74a71fb729644743479c56bb1321b9fc74d978d
300026733 <kumar.swetank@myntra.com>
2021-02-03 22:47:17 +0530

    Changes in project dependency

--- android/get_project_dep.sh
+++ android/get_project_dep.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -rf lib-jitsi-meet/
-git clone https://github.com/myntra/lib-jitsi-meet.git lib-jitsi-meet --branch knuth_changes --single-branch
-git clone https://github.com/myntra/strophejs.git lib-jitsi-meet/strophejs-1.3.4 --branch knuth_changes --single-branch
+rm -rf ../lib-jitsi-meet/
+git clone https://github.com/myntra/lib-jitsi-meet.git ../lib-jitsi-meet --branch knuth_changes --single-branch
+git clone https://github.com/myntra/strophejs.git ../lib-jitsi-meet/strophejs-1.3.4 --branch knuth_changes --single-branch
 
